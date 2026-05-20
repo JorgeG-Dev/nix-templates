085728496dd39105af48ea98fb72333f8ff36754
Jorge Gomez <jgomezmail-dev@proton.me>
2026-05-19 23:05:03 -0400

    added systems

--- flake.nix
+++ flake.nix
@@ -16,6 +16,12 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
       imports = [
         inputs.devshell.flakeModule
         (inputs.import-tree ./shells)
